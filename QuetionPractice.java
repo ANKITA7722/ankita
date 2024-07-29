2f3e963e0551ab7a4e68916d6ba50c9f0f049be0
ANKITA7722 <ANKITA7722>
2024-07-29 17:49:35 +0100

    question

--- QuetionPractice.java
+++ QuetionPractice.java
@@ -8,6 +8,17 @@ class QuetionPractice{
         double result = circumfrance /(2*3.14); 
         System.out.println("redios of circul=" + result) ;*/
 
+        int maximum = 0;
+        int userInput;
+        do{
+            System.out.println("enter an integer ,enter 0 to stop");
+            userInput = scanner.nextInt();
+            if(userInput > maximum){
+                maximum = userInput;
+               
+            }
 
+        }while(userInput != 0);
+        System.out.printing("maximum value =" + maximum);
     }
 }

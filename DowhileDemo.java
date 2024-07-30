aa7a99d5015dc190ef07d69e8afaf97a5e3999ca
ANKITA7722 <ANKITA7722>
2024-07-30 19:06:31 +0100

    java quetion practice

--- DowhileDemo.java
+++ DowhileDemo.java
@@ -3,11 +3,50 @@ import java.util.Scanner;
 class DowhileDemo{
     public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
-        int option;
+       /* int option;
         do{
             System.out.println("enter 1 to start");
             System.out.println("enter 0 to exit");
             option = scanner.nextInt();
-        }while(option != 0);
+        }while(option != 0);*/
+       /* System.out.println("Enter an integer number");
+        int number = scanner.nextInt();
+        int sum = 0;
+        do{
+            sum = sum + number % 10;
+            number = number / 10;
+            number--;
+            //System.out.println(sum);
+        }while(number != 0);
+        System.out.println(sum);*/
+
+//que2:-currect
+        /* System.out.println("Enter an integer number");
+        int number = scanner.nextInt();
+        int sum = 0;
+        do{
+            if(number%2 == 0){
+            System.out.println("even=" + number);
+            }
+            else{
+               System.out.println("odd=" + number); 
+            }
+            number--;
+        }while(number != 0);*/
+
+//que5:-correct
+int number = 1;
+    for(int i=1;i<=20;i++){
+        System.out.println(number*i);
     }
+
+
+//que:-6
+    //int number = 1;
+    for(int i=1;i<=20;i++){
+        if(i%2 != 0){
+        System.out.println("odd"+ i);
+        }
+    }
+ }
 }
--- QuetionPractice.java
+++ QuetionPractice.java
@@ -8,7 +8,7 @@ class QuetionPractice{
         double result = circumfrance /(2*3.14); 
         System.out.println("redios of circul=" + result) ;*/
 
-        int maximum = 0;
+        /*int maximum = 0;
         int userInput;
         do{
             System.out.println("enter an integer ,enter 0 to stop");
@@ -20,5 +20,55 @@ class QuetionPractice{
 
         }while(userInput != 0);
         System.out.printing("maximum value =" + maximum);
-    }
+    }*/
+   //que:-4
+   /*int number1 =scanner.nextInt();
+    int number2 =scanner.nextInt();
+     int number3 =scanner.nextInt();
+     if(number1%9==0 && number2%9==0 && number3%9==0){
+                System.out.println("all nubmers are divisible by 9");
+            }else{
+        System.out.println("numbers not divisble by 9");
+     } */
+
+    //que:-5
+   /* System.out.println("Enter an integer number");
+      int number = scanner.nextInt();
+      if(number != 0){
+        if(number > 0){
+            System.out.println("number is positive");
+        }else{
+            System.out.println("number is negative");
+        }
+      }*/
+
+     //que:-6
+     /*System.out.println("Enter an integer number");
+      int number = scanner.nextInt();
+      if(number != 0){
+        if(number%2 == 0){
+            System.out.println("number is even");
+        }else{
+            System.out.println("number is odd");
+        }
+      }*/
+
+     //que:-8
+
+     // int number1 = scanner.nextInt();
+      //int number2 = scanner.nextInt();
+      System.out.println("Enter an widthdrawl amount");
+      int withdrawalBalance = scanner.nextInt();
+      int curuntbalance = 500;
+      if(withdrawalBalance > curuntbalance){
+        System.out.prinln("insufficient funds");
+        if(withdrawalBalance < curuntbalance){
+            if(curuntbalance <= 500){
+            System.out.println(curuntbalance + 50);
+            }else{
+            System.out.prinln(curuntbalance);
+                }
+        }
+      }
+}
 }

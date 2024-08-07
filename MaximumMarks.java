5786c36b71a4f17cdffc60f94f942c940e74ff79
ANKITA7722 <ANKITA7722>
2024-08-07 19:11:04 +0100

    quetion practice

---
+++ MaximumMarks.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class MaximumMarks{
+    public static void main(String[]args){
+        Scanner scanner = new scanner(System.in);
+        System.out.prinln("enter number of students");
+        int noOfStudent = scanner.nextInt();
+        String tem = scanner.nextInt();
+        int maxScore = 0;
+        
+
+    }
+}
